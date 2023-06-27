734b94dbe55b74eba64d789d4f912e5dc9f01dff
Cadin Batrack <cabatrac@microsoft.com>
2023-06-26 17:02:45 -0700

    Initial Commit

---
+++ AxiControl/AxiControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  AxiControlApp.swift
+//  AxiControl
+//
+//  Created by Cadin Batrack on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct AxiControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
